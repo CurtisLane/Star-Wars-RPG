a83e1b2827d274400af728b099db07d98f912c40
Curtis Lane <clanelmt@gmail.com>
2020-04-15 00:05:50 -0400

    add content, images, styling, and pseudo code. create file for downloaded font.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 // containers for each character. include varied health ponts(hp), attack power(ap)[increases each attack], and counter attack power(cap).
 let lukeSkywalker = {
     hp: 350, //health power, decreases based on ap or cap of attacker.
-    ap: 0, //must increase after each attack, for loop, add 8ap each event(button press, attack)
+    ap: 0, //must increase after each attack, for loop, add 12ap each event(button press, attack)
     cap: 25, // counter attack points, only applies to enemy, does not change
 }
 
@@ -17,7 +17,14 @@ let darthMaul = {
     cap: 55, // counter attack points, only applies to enemy, does not change
 }
 
+let darth = {
+    hp: 600, //health power, decreases based on ap or cap of attacker.
+    ap: 0, //must increase after each attack, for loop, add 4ap each event(button press, attack)
+    cap: 65, // counter attack points, only applies to enemy, does not change
+}
+
+// button press triggers attack function
+// attack function reduces enemy hp by your ap
+
 
 //enemy character only has counter attack power[never changes]
-
-//
